06690a5579d98151573659e77bc8acda2b923d5c
winlin <pcliuguangtao@163.com>
2013-07-26 12:28:21 +0800

    tmp modify

--- LibEventUDPClient/LibEventUDPClient/main.c
+++ LibEventUDPClient/LibEventUDPClient/main.c
@@ -10,23 +10,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define APP_NUMBER    "4"
+
 int main(int argc, const char * argv[])
 {
-    MITLogOpen("UDPClinet", "./logs");
+    MITLogOpen("UDPClinet", "/tmp/logs/LibEventUDPClient"APP_NUMBER);
     
     char dir[1024];
     getcwd(dir, sizeof(dir));
     MITLog_DetPrintf(MITLOG_LEVEL_COMMON, "%s", dir);
     
     struct feed_thread_configure th_conf;
-    th_conf.cmd_line = "./LibEventUDPClient";
-    th_conf.app_name = "AppOne";
-    th_conf.feed_period = 5;
+    th_conf.cmd_line = "/tmp/apps/LibEventUDPClient"APP_NUMBER;
+    th_conf.app_name = "App"APP_NUMBER;
+    th_conf.feed_period = 3;
     th_conf.monitored_pid = getpid();
     MITLog_DetPrintf(MITLOG_LEVEL_COMMON, "Start the feed thread:cmd=%s", th_conf.cmd_line);
     create_feed_thread(&th_conf);
     int i = 0;
-    while (i++ < 300) {
+    while (1) {
         MITLog_DetPuts(MITLOG_LEVEL_COMMON, "The main thread rotate one time per 3 seconds");
         sleep(3);
     }
